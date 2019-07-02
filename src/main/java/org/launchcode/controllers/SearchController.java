1f9b1072d03107475a633cc2124034b1f0d9570e
PriyaC <priyacjose@gmail.com>
2019-07-02 18:18:23 -0500

    updated todo

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,13 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.launchcode.models.JobData.findByValue;
 
 /**
  * Created by LaunchCode
@@ -23,5 +26,24 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value="results", method=RequestMethod.POST)
+    public String processSearch(@RequestParam String searchType,@RequestParam String searchTerm, Model model ) {
+        if (searchType.equals("all")){
+
+            ArrayList<HashMap<String, String>> resultfindByValue=JobData.findByValue(searchTerm );
+            model.addAttribute("jobs",resultfindByValue);
+            return "search";
+        }
+        else {
+
+            ArrayList<HashMap<String, String>> findByColumnAndValue=JobData.findByColumnAndValue(searchType,searchTerm);
+            model.addAttribute("jobs",findByColumnAndValue);
+            return "search";
+            //logic
+        }
+
+    }
+
 
 }
